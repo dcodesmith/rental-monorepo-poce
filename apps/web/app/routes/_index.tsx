8766bf701b427f504417951c9d4e59fac1de7295
Afees Kolawole <dcodesmith@gmail.com>
2024-01-02 08:46:29 +0100

    render data from api

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -20,12 +20,12 @@ export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Welcome to Remix</h1>
-      {/* <code>
+      <code>
         {JSON.stringify(data, null, 2)}
       </code>
       <pre>
         {JSON.stringify(data, null, 2)}
-      </pre> */}
+      </pre>
       <ul>
         <li>
           <a
